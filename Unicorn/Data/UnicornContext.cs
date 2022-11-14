5b6150819161e40dfadf99cc4305d74142403228
HelenAXS <helen.galan@gmail.com>
2022-11-14 14:09:43 +0100

    Add project files.

---
+++ Unicorn/Data/UnicornContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Unicorn.Models;
+
+namespace Unicorn.Data
+{
+    public class UnicornContext : DbContext
+    {
+        public UnicornContext (DbContextOptions<UnicornContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Unicorn.Models.Department> Department { get; set; } = default!;
+    }
+}
---
+++ Unicorn/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace Unicorn.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
