5684f0983f6f5bec791ee521884a8c303dc33d40
HelenGalan <helen.galan@gmail.com>
2022-11-22 12:44:45 +0100

    Issue with Startup and Program

---
+++ Unicorn/Data/SeedingService.cs
@@ -0,0 +1,29 @@
+﻿using Unicorn.Models;
+
+namespace Unicorn.Data
+{
+    //quando um SeedingService for criado, vai receber uma instancia do context tambem
+    public class SeedingService
+    {
+        private UnicornContext _context;
+
+        public SeedingService(UnicornContext context)
+        {
+            _context = context;
+        }
+
+        //operacao responsavel por popular a BD
+        public void Seed()
+        {
+            //if para testar se existe algum dado na base de dados (se existir, nao faz nada)
+            if (_context.Department.Any() ||
+                _context.Seller.Any() ||
+                _context.SalesRecord.Any()) //esta testando as tres tabelas com a operacao any do linq
+            {
+                return; //para cortar a execucao do metodo, nao faz mais nada, pois o BD ja foi populado
+            }
+
+            Department d1
+        }
+    }
+}
--- Unicorn/Program.cs
+++ Unicorn/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddDbContext<UnicornContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +18,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    //verificar com alguem onde fica, pois esta no Startup
+    SeedingService.Seed();
 }
 
 app.UseHttpsRedirection();
