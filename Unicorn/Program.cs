893c35367c49d35142e1ed195665ec5ee941fa14
HelenGalan <helen.galan@gmail.com>
2022-11-24 14:57:11 +0100

    SellersController e Seeding que não deu certo

--- Unicorn/Program.cs
+++ Unicorn/Program.cs
@@ -1,16 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Unicorn.Data;
+using Unicorn.Models.Enums;
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<UnicornContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("UnicornContext") ?? throw new InvalidOperationException("Connection string 'UnicornContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddScoped<SeedingService>();
+//builder.Services.AddTransient<SeedingService>(); //mudei para AddTransient //nao funcionou tambem
+
+builder.Services.AddDbContext<UnicornContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("UnicornContext") ?? throw new InvalidOperationException("Connection string 'UnicornContext' not found.")));
+
 
 var app = builder.Build();
+
+/*
+Nao funcionou, estou usando outra forma 
+
+//aqui comeca o seeding operation
+if (args.Length == 1 && args[0].ToLower() == "SeedingService")
+    SeedData(app);
+
+void SeedData(IHost app)
+{
+    var scopedFactory = app.Services.GetServices<IServiceScopeFactory>();
+
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<SeedingService>;
+        service.Seed();
+    }
+}
+//aqui termina
+*/
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,7 +47,8 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 
     //verificar com alguem onde fica, pois esta no Startup
-    SeedingService.Seed();
+    //SeedingService.Seed(); //nao vou usar mais
+        
 }
 
 app.UseHttpsRedirection();
@@ -35,3 +63,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+UnicornDbInitializer.Seed(app);
